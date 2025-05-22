6ecd92ab45e4bf20ef315c6d97bd956f51bbf450
Mahmuda Begum <mahmuda.begum@v2solutions.com>
2025-05-22 11:39:29 +0530

    Release version 1.0.0

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'node:path'
 
 const isGH = process.env.NODE_ENV === 'production'
 
@@ -13,6 +14,25 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  build: {
+    sourcemap: true,
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: 'VueTailwindDesignSystem',
+      fileName: (format) => `vue-tailwind-design-system.${format}.js`,
+      formats: ['es', 'cjs'],
+    },
+    rollupOptions: {
+      external: ['vue', 'pinia', 'vue-router'],
+      output: {
+        globals: {
+          vue: 'Vue',
+          pinia: 'Pinia',
+          'vue-router': 'VueRouter',
+        },
+      },
     },
   },
 })
